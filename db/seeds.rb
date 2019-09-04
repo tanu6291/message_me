aae0bab04f4ea13ca7b9358dc2ae8c6ab2481f6c
Rails Udemy course-Tanu <railsudemycoursetanu@gmail.com>
2019-09-04 19:19:37 +0530

    Add session, chatroom and message controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Shyam", password: "password")
+User.create(username: "Kapil", password: "password")
+User.create(username: "Divyanshu", password: "password")
+User.create(username: "Kriti", password: "password")
+User.create(username: "Indra", password: "password")
+
+
+Message.create(body: "Hie this is second message", user: User.first)
+Message.create(body: "Hie to the Lets chat", user: User.last)
